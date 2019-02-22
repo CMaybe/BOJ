2a0283f1cfbb1179dff8c5c36384b6dc475ce6a9
jaegyeom <asbsds123@gmail.com>
2019-02-22 23:44:18 +0900

    change

--- 1003.c
+++ 1003.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
+
+int arr[41][2];
+
 int main()
 {
-	int n,a,arr[42] = { 1,0 };
-	for (int i = 2; i < 42; i++) {
-		arr[i] = arr[i - 1] + arr[i - 2];
+	int a, n;
+	arr[0][0] = 1;
+	arr[1][1] = 1;
+	scanf("%d", &n);
+	for (int i = 2; i <= 40; i++) {
+		arr[i][0] = arr[i - 1][0] + arr[i - 2][0];
+		arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
 	}
-	for (scanf("%d", &n); n > 0; n--) {
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &a);
-		printf("%d %d\n", arr[a], arr[a+1]);
+		printf("%d %d\n", arr[a][0], arr[a][1]);
 	}
+
 }
