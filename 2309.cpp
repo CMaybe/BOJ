b8a175eb91e61b5cf54134b89f5d6d59cd7d8cbe
Maybe <asbsds123@gmail.com>
2021-01-10 13:19:44 +0900

    Add files via upload

---
+++ 2309.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int arr[10];
+
+int main(int argc, char** argv) {
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(0);
+	int sum=0;
+	for(int i =0;i<9;i++){
+		cin>>arr[i];
+		sum+=arr[i];
+	}
+	for(int i =0;i<9;i++){
+		for(int j =i+1;j<9;j++){
+			
+		}
+	}
+}
+
+
