ad9791f01942f2eb22389a689e75f09c117ac41f
Maybe <asbsds123@gmail.com>
2021-01-17 14:51:34 +0900

    Create 15655.cpp

---
+++ 15655.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n,m;
+vector<int> v;
+bool visit[10];
+int arr[10];
+void func(int cnt,int idx) {
+	if (cnt == m) {
+		for (int i=0; i<v.size(); i++){
+		    cout<<v[i]<<" ";
+		}
+		cout<<"\n";
+		return;
+	}
+	
+	for (int i=idx; i<n; i++) {
+	    if(visit[i]) continue;
+		v.push_back(arr[i]);
+		visit[i]=true;
+		func(cnt+1,i);
+		v.pop_back();
+		visit[i]=false;
+	}
+}
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+	cin>>n>>m;
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	func(0,0);
+	return 0;	
+}
