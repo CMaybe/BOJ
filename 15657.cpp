540f3f5393e588d4547bb4c4754a88f162fa5508
Maybe <asbsds123@gmail.com>
2021-01-17 14:54:59 +0900

    Create 15657.cpp

---
+++ 15657.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n,m;
+vector<int> v;
+int arr[10];
+void func(int cnt,int idx) {
+	if (cnt == m) {
+		for (int i=0; i<v.size(); i++){
+		    cout<<v[i]<<" ";
+		}
+		cout<<"\n";
+		return;
+	}
+	
+	for (int i=idx; i<n; i++) {
+		v.push_back(arr[i]);
+		func(cnt+1,i);
+		v.pop_back();
+	}
+}
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+	cin>>n>>m;
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	func(0,0);
+	return 0;	
+}
