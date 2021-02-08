4199ac86d4ea46a040c75fe59f2b4a3e62fddd35
Maybe <asbsds123@gmail.com>
2021-02-08 23:38:08 +0900

    Update 1074.cpp

--- 1074.cpp
+++ 1074.cpp
@@ -1,41 +1,25 @@
-#include<cstdio>
+#include <iostream>
 #include<cmath>
-
-int gr, gc, cnt;
-void func(int n, int r, int c)
-{
-	if (n == 2) {
-		if (r == gr && c == gc) {
-			printf("%d", cnt);
-			return;
-		}
-		cnt++;
-		if (r == gr && c + 1 == gc) {
-			printf("%d", cnt);
-			return;
-		}
-		cnt++;
-		if (r+1 == gr && c == gc) {
-			printf("%d", cnt);
-			return;
-		}
-		cnt++;
-		if (r+1 == gr && c + 1 == gc) {
-			printf("%d", cnt);
-			return;
-		}
-		cnt++;
-		return;
-	}
-	func(n / 2, r, c);
-	func(n / 2, r, c + n / 2);
-	func(n / 2, r + n / 2, c);
-	func(n / 2, r + n / 2, c + n / 2);
-}
+using namespace std;
 
 int main()
 {
-	int size;
-	scanf("%d %d %d", &size, &gr, &gc);
-	func(pow(2, size), 0, 0);
+    int n,r,c;
+    cin>>n>>r>>c;
+    int ans=0;
+    for(int i  =n;i>0;i--){
+        int t;
+        int s =(int)pow(2,i)/2;
+        if(c<s&&r<s) t=0;
+        else if(c>=s&&r<s) t=1;
+        else if(c<s&&r>=s) t=2;
+        else if(c>=s&&r>=s) t=3;
+        
+        c%=s;
+        r%=s;
+        ans+=(int)pow(s,2)*t;
+    }
+    cout<<ans;
+
+    return 0;
 }
