12e63243822b450791028258288b907fce6d276e
Maybe <asbsds123@gmail.com>
2021-02-15 18:01:25 +0900

    Create 11655.cpp

---
+++ 11655.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin,s);
+    for(int i =0;i<s.length();i++){
+        if('a'<=s[i] && s[i]<='z'){
+            s[i]= ((s[i]-'a')+13)%26 +'a';
+        }
+        else if('A'<=s[i] && s[i]<='Z'){
+            s[i]= ((s[i]-'A')+13)%26 +'A';
+        }
+    }
+    
+    cout<<s;
+
+    return 0;
+}
