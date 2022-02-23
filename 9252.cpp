976a275d93112f65a52e00084f7a8982c6f8e102
Jaegyeom Kim <asbsds123@gmail.com>
2022-02-23 11:11:19 +0900

    Update 9252.cpp

--- 9252.cpp
+++ 9252.cpp
@@ -1,42 +1,45 @@
 #include<iostream>
 #include<string>
+#include<stack>
 #include<algorithm>
 
 using namespace std;
 
 int arr[1002][1002];
-string s[1002][1002];
+
 int main()
 {
 	cin.tie(0);
 	cin.sync_with_stdio(false);
 	string s1, s2;
 	cin >> s1 >> s2;
-	for(int i = 0;i<1002;i++){
-		for(int j = 0;j<1002;j++){
-			s[i][j]= "";
-		}
-	}
 	for (int i = 1; i <= s1.length(); i++) {
 		for (int j = 1; j <= s2.length(); j++) {
-			if (s1[i - 1] == s2[j - 1]){
+			if (s1[i - 1] == s2[j - 1]) {
 				arr[i][j] = arr[i - 1][j - 1] + 1;
-				s[i][j] = s[i-1][j-1] + s1[i-1];
 			}
 			else {
-				if(arr[i - 1][j] > arr[i][j - 1]){
-					arr[i][j] = arr[i - 1][j];
-					s[i][j] = s[i-1][j];
-				}
-				else{
-					arr[i][j] = arr[i][j-1];
-					s[i][j] = s[i][j-1];
-				}
+				arr[i][j] = max(arr[i - 1][j], arr[i][j - 1]);
 			}
 		}
 	}
 
-	cout << arr[s1.length()][s2.length()]<<'\n';
-	if(arr[s1.length()][s2.length()]>0)cout<<s[s1.length()][s2.length()];
+	stack<char> st;
+	int i = s1.length(), j = s2.length();
+	while (i > 0 && j > 0) {
+		if (arr[i][j] == arr[i][j - 1]) j--;
+		else if (arr[i][j] == arr[i - 1][j])i--;
+		else {
+			i--;
+			j--;
+			st.push(s1[i]);
+		}
+	}
+
+	cout << arr[s1.length()][s2.length()] << '\n';
+	while (!st.empty()) {
+		cout << st.top();
+		st.pop();
+	}
 	return 0;
 }
