ab1e0e9331eabac3a1fffb5cad6561587f34364e
Jaegyeom Kim <asbsds123@gmail.com>
2022-08-27 11:39:48 +0900

    Create 25083.cpp

---
+++ 25083.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	std::cout << "         ,r'\"7\n";
+	std::cout << "r`-_   ,'  ,/\n";
+	std::cout << " \\. \". L_r'\n";
+	std::cout << "   `~\\/\n";
+	std::cout << "      |\n";
+	std::cout << "      |\n";
+	return 0;
+}
